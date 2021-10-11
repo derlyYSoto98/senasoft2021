a7eef5404789570c3e84934472048ef995582896
EvelinBermeo <evelinbermeo05@gmail.com>
2021-10-11 17:39:36 -0500

    Subiendo registros

---
+++ registro_inventario.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Registro de Inventario</h1>
+    <form action="post" action="">
+        
+    </form>
+</body>
+</html>
